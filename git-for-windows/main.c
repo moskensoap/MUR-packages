fcab7fd54623b014b3d4f1c39a9b22a7f2943a07
moskensoap <141073988+moskensoap@users.noreply.github.com>
2024-07-30 16:00:04 +0000

    new package: git-for-windows

---
+++ git-for-windows/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <shlwapi.h>  // For PathFindFileName, PathCchRemoveFileSpec, PathCchCombine
+
+#pragma comment(lib, "shlwapi.lib")  // Link with Shlwapi.lib
+
+int main(int argc, char *argv[]) {
+    char path[MAX_PATH];
+    if (GetModuleFileName(NULL, path, MAX_PATH) == 0) {
+        perror("GetModuleFileName");
+        return 1;
+    }
+
+    char filePath[MAX_PATH];
+    strcpy(filePath, __FILE__);
+
+    // Get the file name from the file path
+    char fileName[MAX_PATH];
+    strcpy(fileName, PathFindFileName(__FILE__));
+
+    // Remove the file extension
+    char *dot = strrchr(fileName, '.');
+    if (dot) {
+        *dot = '\0';
+    }
+
+    // Build the paths
+    char binPath[MAX_PATH], realPath[MAX_PATH];
+    sprintf(binPath, "\\bin\\%s.exe", fileName);
+    sprintf(realPath, "\\share\\mur__git-for-windows\\cmd\\%s", fileName);
+
+    // Replace binPath with realPath in path
+    char *replacePtr = strstr(path, binPath);
+    if (replacePtr != NULL) {
+        strncpy(replacePtr, realPath, strlen(realPath));
+        replacePtr += strlen(realPath);
+        *replacePtr = '\0';
+    } else {
+        perror("strstr");
+        return 1;
+    }
+
+    size_t total_length = 0;
+    size_t hx_length = strlen(path) + 5;
+
+    for (int i = 1; i < argc; i++) {
+        total_length += strlen(argv[i]) + 3;
+    }
+
+    char merged_string[total_length + hx_length];
+    char *ptr = merged_string;
+
+    *ptr++ = '"';
+    *ptr++ = '"';
+    strcpy(ptr, path);
+    ptr += strlen(path);
+    *ptr++ = '"';
+    if (argc == 1) {
+        *ptr++ = '"';
+        *ptr = '\0';
+        return system(merged_string);
+    } else {
+        *ptr++ = ' ';
+    }
+
+    for (int i = 1; i < argc; i++) {
+        *ptr++ = '"';
+        strcpy(ptr, argv[i]);
+        ptr += strlen(argv[i]);
+        *ptr++ = '"';
+        if (i < argc - 1) {
+            *ptr++ = ' ';
+        }
+    }
+
+    *ptr++ = '"';
+    *ptr = '\0';
+    return system(merged_string);
+}

cec139320572c0ba9cbc718820a728fe351e1df8
moskensoap <141073988+moskensoap@users.noreply.github.com>
2024-08-18 10:42:34 +0000

    new package: git-vscode

---
+++ git-vscode/main.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    char path[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", path, sizeof(path) - 1);
+
+    if (count == -1)
+    {
+        perror("readlink");
+        return 1;
+    }
+
+    path[count] = '\0'; // Null-terminate the path
+
+    // change all characters to lowercase in path
+    for (int i = 0; path[i]; i++)
+    {
+        path[i] = tolower(path[i]);
+    }
+
+    // Replace "/usr/local/bin/git" with "/usr/bin/git"
+    char *binPath = "/usr/local/bin/git";
+    char *realPath = "/usr/bin/git";
+    char *replacePtr = strstr(path, binPath);
+    if (replacePtr != NULL)
+    {
+        strncpy(replacePtr, realPath, strlen(realPath));
+        replacePtr += strlen(realPath);
+        *replacePtr = '\0';
+    }
+    else
+    {
+        perror("strstr");
+        return 1;
+    }
+
+    char cygpath[PATH_MAX];
+    strcpy(cygpath, path);
+    // Replace "/usr/bin/git" with "/usr/bin/cygpath"
+    binPath = "/usr/bin/git";
+    realPath = "/usr/bin/cygpath";
+    replacePtr = strstr(cygpath, binPath);
+    if (replacePtr != NULL)
+    {
+        strncpy(replacePtr, realPath, strlen(realPath));
+        replacePtr += strlen(realPath);
+        *replacePtr = '\0';
+    }
+    else
+    {
+        perror("strstr");
+        return 1;
+    }
+
+    size_t total_length = 0;
+    size_t path_length = strlen(path) + 3;
+
+    for (int i = 1; i < argc; i++)
+    {
+        total_length += strlen(argv[i]) + 3;
+    }
+
+    char merged_string[total_length + path_length];
+    char *ptr = merged_string;
+
+    *ptr++ = '"';
+    strcpy(ptr, path);
+    ptr += strlen(path);
+    *ptr++ = '"';
+    if (argc == 1)
+    {
+        *ptr = '\0';
+        return system(merged_string);
+    }
+    else
+    {
+        *ptr++ = ' ';
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        *ptr++ = '"';
+        strcpy(ptr, argv[i]);
+        ptr += strlen(argv[i]);
+        *ptr++ = '"';
+        if (i < argc - 1)
+        {
+            *ptr++ = ' ';
+        }
+    }
+
+    *ptr = '\0';
+
+    if (argc >= 2 && strcmp(argv[1], "rev-parse") == 0)
+    {
+        FILE *fp = popen(merged_string, "r");
+        if (fp == NULL)
+        {
+            perror("popen");
+            return 1;
+        }
+
+        char *line = NULL;
+        size_t len = 0;
+        ssize_t read;
+
+        while ((read = getline(&line, &len, fp)) != -1)
+        {
+            if (line[read - 1] == '\n')
+            {
+                line[read - 1] = '\0';
+            }
+            char cygpath_merged_string[PATH_MAX * 2 + 5];
+            sprintf(cygpath_merged_string, "%s -w %s", cygpath, line);
+            system(cygpath_merged_string);
+        }
+
+        free(line);
+        pclose(fp);
+        return 0;
+    }
+
+    return system(merged_string);
+}

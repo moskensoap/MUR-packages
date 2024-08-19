7bd6a3506f8cc81bed9297fbf55243162503a9b1
moskensoap <141073988+moskensoap@users.noreply.github.com>
2024-08-19 15:17:46 +0000

    fix(makelink): pass exit code of original mingw32-make to the wrapper and return

--- makelink/main.c
+++ makelink/main.c
@@ -1,15 +1,15 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     size_t total_length = 0;
     size_t mingw_length = strlen("mingw32-make.exe") + 1;
 
-    if(argc==1){
-        return system("mingw32-make.exe");
-    }
-
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
         total_length += strlen(argv[i]) + 3;
     }
 
@@ -18,19 +18,54 @@ int main(int argc, char *argv[]) {
 
     strcpy(ptr, "mingw32-make.exe");
     ptr += strlen("mingw32-make.exe");
-    *ptr++ = ' ';
+    if (argc == 1)
+    {
+        *ptr = '\0';
+    }
+    else
+    {
+        *ptr++ = ' ';
+    }
 
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
         *ptr++ = '"';
         strcpy(ptr, argv[i]);
         ptr += strlen(argv[i]);
         *ptr++ = '"';
-        if (i < argc - 1) {
+        if (i < argc - 1)
+        {
             *ptr++ = ' ';
         }
     }
 
     *ptr = '\0';
 
-    return system(merged_string);
+    // Use CreateProcess to execute the command and capture its exit code
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    memset(&si, 0, sizeof(si));
+    si.cb = sizeof(si);
+    memset(&pi, 0, sizeof(pi));
+
+    if (!CreateProcess(NULL, merged_string, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+    {
+        fprintf(stderr, "CreateProcess failed (%d).\n", GetLastError());
+        return 1;
+    }
+
+    // Wait for the process to finish and get the exit code
+    DWORD exitCode;
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    if (!GetExitCodeProcess(pi.hProcess, &exitCode))
+    {
+        fprintf(stderr, "GetExitCodeProcess failed (%d).\n", GetLastError());
+        exitCode = 1;
+    }
+
+    // Clean up
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return exitCode;
 }

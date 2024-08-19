ac39c0c99f0d08f57649d9d231171c4744db5490
moskensoap <141073988+moskensoap@users.noreply.github.com>
2024-08-19 14:39:42 +0000

    fix(git-for-windows): pass exit code of original git to the wrapper

--- git-for-windows/main.c
+++ git-for-windows/main.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#include <shlwapi.h>  // For PathFindFileName, PathCchRemoveFileSpec, PathCchCombine
+#include <shlwapi.h> // For PathFindFileName, PathCchRemoveFileSpec, PathCchCombine
 
-#pragma comment(lib, "shlwapi.lib")  // Link with Shlwapi.lib
+#pragma comment(lib, "shlwapi.lib") // Link with Shlwapi.lib
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     char path[MAX_PATH];
-    if (GetModuleFileName(NULL, path, MAX_PATH) == 0) {
+    if (GetModuleFileName(NULL, path, MAX_PATH) == 0)
+    {
         perror("GetModuleFileName");
         return 1;
     }
@@ -22,7 +24,8 @@ int main(int argc, char *argv[]) {
 
     // Remove the file extension
     char *dot = strrchr(fileName, '.');
-    if (dot) {
+    if (dot)
+    {
         *dot = '\0';
     }
 
@@ -33,49 +36,82 @@ int main(int argc, char *argv[]) {
 
     // Replace binPath with realPath in path
     char *replacePtr = strstr(path, binPath);
-    if (replacePtr != NULL) {
+    if (replacePtr != NULL)
+    {
         strncpy(replacePtr, realPath, strlen(realPath));
         replacePtr += strlen(realPath);
         *replacePtr = '\0';
-    } else {
+    }
+    else
+    {
         perror("strstr");
         return 1;
     }
 
     size_t total_length = 0;
-    size_t hx_length = strlen(path) + 5;
+    size_t cmd_length = strlen(path) + 3;
 
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
         total_length += strlen(argv[i]) + 3;
     }
 
-    char merged_string[total_length + hx_length];
+    char merged_string[total_length + cmd_length];
     char *ptr = merged_string;
 
-    *ptr++ = '"';
     *ptr++ = '"';
     strcpy(ptr, path);
     ptr += strlen(path);
     *ptr++ = '"';
-    if (argc == 1) {
+    if (argc == 1)
+    {
         *ptr++ = '"';
         *ptr = '\0';
-        return system(merged_string);
-    } else {
+    }
+    else
+    {
         *ptr++ = ' ';
     }
 
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
         *ptr++ = '"';
         strcpy(ptr, argv[i]);
         ptr += strlen(argv[i]);
         *ptr++ = '"';
-        if (i < argc - 1) {
+        if (i < argc - 1)
+        {
             *ptr++ = ' ';
         }
     }
 
-    *ptr++ = '"';
     *ptr = '\0';
-    return system(merged_string);
+
+    // Use CreateProcess to execute the command and capture its exit code
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    memset(&si, 0, sizeof(si));
+    si.cb = sizeof(si);
+    memset(&pi, 0, sizeof(pi));
+
+    if (!CreateProcess(NULL, merged_string, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+    {
+        fprintf(stderr, "CreateProcess failed (%d).\n", GetLastError());
+        return 1;
+    }
+
+    // Wait for the process to finish and get the exit code
+    DWORD exitCode;
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    if (!GetExitCodeProcess(pi.hProcess, &exitCode))
+    {
+        fprintf(stderr, "GetExitCodeProcess failed (%d).\n", GetLastError());
+        exitCode = 1;
+    }
+
+    // Clean up
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return exitCode;
 }
